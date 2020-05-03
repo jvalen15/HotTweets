525193d9954846869841ac523ef7ee35820feaf6
Brian Stone <bstone26@uncc.edu>
2020-05-03 16:55:43 -0400

    can now sign into twitter

--- hot-tweets/app.js
+++ hot-tweets/app.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
 let navigationController = require('./controller/navigationController');
 //let sessionController = require('./controller/sessionController');
 
-var app = express();
+//
+passport.use(new Strategy({
+    consumerKey : 'Mc3zVkWyMBmVMzPncjqj5lVSW',
+    consumerSecret:'3nyl4RNtbpwiGl0RjbaKlgfWv2o2HjU0TBKZy5ZFgZ2tkc7HYk',
+    callbackURL:'http://localhost:8080/return/twitter'
+}, function(token, tokenSecret, profile, callback){
+    return callback(null, profile);
+}
+));
+passport.serializeUser(function(user, callback) {
+    callback(null, user);
+});
 
+passport.deserializeUser(function(obj, callback) {
+    callback(null, obj);
+});
+//
+
+
+var app = express();
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({secret: 'whatever', resave: true, saveUninitialized: true}))
 
-app.use(session({secret: 'something'}));
+//app.use(session({secret: 'something'}));
 
 //app.use('/session', sessionController);
 var ageVerified = false;
--- hot-tweets/controller/navigationController.js
+++ hot-tweets/controller/navigationController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
+var passport = require('passport');
 var tweetDb = require('../utility/tweetDB');
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -13,8 +14,10 @@ router.get('/', function(req,res){
     //console.log(dd);
     //var mm = today.getMonth()+1;
     //var yyyy = today.getFullYear();
+    //console.log(req.user);
+    var user = req.session.user;
     tweets.sort((a,b) => (a.favorite_count < b.favorite_count) ? 1 : -1);
-    res.render('index',{tweets:tweets, ageVerified:req.session.ageVerified});
+    res.render('index',{tweets:tweets, ageVerified:req.session.ageVerified, user:user});
 });
 router.get('/verifyAge', function(req,res){
     console.log("submitted");
@@ -25,13 +28,16 @@ router.get('/day', function(req,res){
     var tweets = tweetDb.getTweets();
     var todayTweets = [];
     var today = new Date().getDate();
+    var thisMonth = new Date().getMonth();
+    var thisYear = new Date().getFullYear();
+    var user = req.session.user;
     for(var i = 0; i < tweets.length; i++){
-        if(tweets[i].publish_date.getDate() == today){
+        if(tweets[i].publish_date.getDate() == today && tweets[i].publish_date.getMonth() == thisMonth && tweets[i].publish_date.getFullYear() == thisYear){
             todayTweets.push(tweets[i]);
         }
     }
     todayTweets.sort((a,b) => (a.favorite_count < b.favorite_count) ? 1 : -1);
-    res.render('index',{tweets:todayTweets, ageVerified:req.session.ageVerified});
+    res.render('index',{tweets:todayTweets, ageVerified:req.session.ageVerified, user:user});
 });
 // router.get('/week', function(req,res){
 //     var tweets = tweetDb.getTweets();
@@ -50,18 +56,21 @@ router.get('/month', function(req,res){
     var tweets = tweetDb.getTweets();
     var monthTweets = [];
     var thisMonth = new Date().getMonth();
+    var thisYear = new Date().getFullYear();
+    var user = req.session.user;
     for(var i = 0; i < tweets.length; i++){
-        if(tweets[i].publish_date.getMonth() == thisMonth){
+        if(tweets[i].publish_date.getMonth() == thisMonth && tweets[i].publish_date.getFullYear() == thisYear){
             monthTweets.push(tweets[i]);
         }
     }
     monthTweets.sort((a,b) => (a.favorite_count < b.favorite_count) ? 1 : -1);
-    res.render('index',{tweets:monthTweets, ageVerified:req.session.ageVerified});
+    res.render('index',{tweets:monthTweets, ageVerified:req.session.ageVerified, user:user});
 });
 router.get('/year', function(req,res){
     var tweets = tweetDb.getTweets();
     var yearTweets = [];
     var thisYear = new Date().getFullYear();
+    var user = req.session.user;
     for(var i = 0; i < tweets.length; i++){
         if(tweets[i].publish_date.getFullYear() == thisYear){
             yearTweets.push(tweets[i]);
@@ -69,13 +78,23 @@ router.get('/year', function(req,res){
     }
     //yearTweets.sort(function(a.favorite_count, b.favorite_count){return b - a});
     yearTweets.sort((a,b) => (a.favorite_count < b.favorite_count) ? 1 : -1);
-    res.render('index',{tweets:yearTweets, ageVerified:req.session.ageVerified});
+    res.render('index',{tweets:yearTweets, ageVerified:req.session.ageVerified, user:user});
 });
 router.get('/allTime', function(req,res){
     var tweets = tweetDb.getTweets();
-    
+    var user = req.session.user;
     tweets.sort((a,b) => (a.favorite_count < b.favorite_count) ? 1 : -1);
-    res.render('index',{tweets:tweets, ageVerified:req.session.ageVerified});
+    res.render('index',{tweets:tweets, ageVerified:req.session.ageVerified, user:user});
+});
+router.get('/twitter/login', passport.authenticate('twitter'));
+
+router.get('/return/twitter', passport.authenticate('twitter', {
+    failureRedirect: '/'
+}), function(req,res){
+    // console.log("success");
+    // console.log(req.user.username);
+    req.session.user = req.user;
+    res.redirect('/');
 });
 
 module.exports = router;
